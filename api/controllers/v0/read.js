af01cbf4899dfce7e0485156d3825c4a13af6005
GinMu <gin.musheng@gmail.com>
2020-04-01 13:17:35 +0800

    read api

--- api/controllers/v0/read.js
+++ api/controllers/v0/read.js
@@ -1,3 +1,5 @@
+const ipfs = require('@utils/ipfs');
+
 module.exports = {
   friendlyName: 'Read',
   description: 'Read data with given hash from ipfs.',
@@ -10,10 +12,41 @@ module.exports = {
   },
 
   exits: {},
-  async fn(inputs) {
-    // All done.
-    console.log(inputs);
-
-    return;
+  async fn({ hash }) {
+    try {
+      const files = await ipfs.get(hash);
+      const results = [];
+      for await (file of files) {
+        const content = [];
+        for await (const chunk of file.content) {
+          content.push(chunk);
+        }
+        const res = JSON.parse(content.toString());
+        results.push({
+          data: res.data,
+          md5: res.md5,
+          size: res.size,
+          name: res.name,
+          sign: {
+            md5: res.md5,
+            size: res.size,
+            name: res.name,
+            timestamp: res.timestamp
+          },
+          timestamp: res.timestamp,
+          publickey: res.publickey
+        });
+      }
+      return {
+        success: true,
+        results
+      };
+    } catch (error) {
+      sails.log(error);
+      return {
+        success: false,
+        msg: error.message
+      };
+    }
   }
 };
