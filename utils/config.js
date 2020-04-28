f0c60af61036f6164235adf710d660c2854da279
zCaviare <194108668@qq.com>
2020-04-28 16:37:17 +0800

    Create config.js

---
+++ utils/config.js
@@ -0,0 +1,28 @@
+/**
+ * 运营商
+ * @return {{address: string 钱包地址, secret: string 密钥}}
+ */
+const operator = () => {
+  return {
+    address: 'j4M4AoSi522XxNpywfyBahmjzQihc4EegL',
+    secret: 'sa9UcyBBD3A3JU3Ux3ZKcbNCxVw9h'
+  };
+};
+
+/**
+ * 辅助运营商
+ * @return {{address: string 钱包地址}}
+ */
+const spareOperator = () => {
+  return {
+    address: 'jHDbFiFZ6rfDjhfRnhD1ReCwY2erhpiYBS'
+  };
+};
+
+const charge_amount = 1000; //用户充值金额
+
+module.exports = {
+  operator,
+  spareOperator,
+  charge_amount
+};
